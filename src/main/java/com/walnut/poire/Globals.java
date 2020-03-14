8acd95471a28fa2a56614f7f0c717e8f45a3a166
Siri Kumar <ssirekumar@gmail.com>
2020-03-14 21:14:33 +0530

    Update Globals.java

--- src/main/java/com/walnut/poire/Globals.java
+++ src/main/java/com/walnut/poire/Globals.java
@@ -2,16 +2,14 @@ package com.walnut.poire;
 
 /**
  * All constants with Global scope across framework are created in this interface
- * @author Siri Kumar Puttagunta*/
+ * @author Siri Kumar Puttagunta
+ */
 public interface Globals {
-
 	/*
 	 * Author : Siri Kumar Puttagunta,
-	 * email: ssirekumar@gmail.com 
-	 * Date : 01 Oct 2013, 
+	 * email: ssirekumar@gmail.com
 	 * All Global constants for entire framework.
 	 */
-	
 	//public static enum Priority {ALL, CRITICAL, HIGH, MEDIUM, LOW};
 	//public static enum Logger {PASS, WARNING, FAIL, CAPTURE, INFO};
 	public static enum Log {INFO, DEBUG, ERROR, FATAL, WARN};
